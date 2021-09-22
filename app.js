5d37364e8380fd14fd1c4dfaad00c7ad0fe47e2f
Quadri Yusuf <quadriy@ebanqo.com>
2021-09-22 04:06:26 +0100

    functional at least

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ var main = function () {
 	"use strict";
 
 	$(".comment-input button").on("click", function (event){
-		var $new_comment = $("<p>").text("this is a new comment");
+		var $new_comment = $("<p>").text($(".comment-input input").val());
 
 		$(".comments").append($new_comment);
 	});
 };
 
 $(document).ready(main);
+
+
